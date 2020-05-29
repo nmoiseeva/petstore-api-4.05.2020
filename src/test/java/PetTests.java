776b1e088fe27e4c3a830ee9082af65fac449ff3
nmoiseeva <31573330+nmoiseeva@users.noreply.github.com>
2020-05-29 17:11:25 +0300

    lesson 2

--- src/test/java/PetTests.java
+++ src/test/java/PetTests.java
@@ -1,20 +1,27 @@
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
+import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
-import org.junit.Assert;
 import org.junit.Test;
-
-import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
+
 
 public class PetTests {
 
-    @Test
-    public void getPetById(){
-        given()
+    private RequestSpecification given (){
+     return RestAssured.
+                given()
+                .contentType("application/json")
                 .log().all()
                 .baseUri("https://petstore.swagger.io/v2")
-                .get("/pet/1")
+                .contentType("application/json");
+
+    }
+
+    @Test
+    public void getPetById(){
+     given()
+                .get(PetEndpoint.GET_PET)
                 .then()
                 .log().all()
                 .statusCode(200);
@@ -23,31 +30,26 @@ public class PetTests {
 
     @Test
     public void createPet (){
-        String bodyJson = "{\n" +
-                "\"id\":1,\n" +
-                "\"category\":{\n" +
-                "\"id\":1,\n" +
-                "\"name\":\"CatName\"\n" +
-                "},\n" +
-                "\"name\":\"CatName\",\n" +
-                "\"photoUrls\":[\n" +
-                "\"string\"\n" +
-                "],\n" +
-                "\"tags\":[\n" +
-                "{\n" +
-                "\"id\":1,\n" +
-                "\"name\":\"CatName\"\n" +
-                "}\n" +
-                "],\n" +
-                "\"status\":\"available\"\n" +
-                "}";
-
-        RequestSpecification request = new RequestSpecBuilder()
-                .setBaseUri("https://petstore.swagger.io/v2")
-                .build();
-
-        given(request).body(bodyJson).contentType(ContentType.JSON).log().all().
-                when().post("/pet").
+       given()
+               .body("{\n" +
+                       "\"id\":1,\n" +
+                       "\"category\":{\n" +
+                       "\"id\":1,\n" +
+                       "\"name\":\"CatName\"\n" +
+                       "},\n" +
+                       "\"name\":\"CatName\",\n" +
+                       "\"photoUrls\":[\n" +
+                       "\"string\"\n" +
+                       "],\n" +
+                       "\"tags\":[\n" +
+                       "{\n" +
+                       "\"id\":1,\n" +
+                       "\"name\":\"CatName\"\n" +
+                       "}\n" +
+                       "],\n" +
+                       "\"status\":\"available\"\n" +
+                       "}")
+                .post(PetEndpoint.CREATE_PET, 1).
                 then().log().body().
                 body("id", equalTo(1)).
                 statusCode(200);
@@ -55,31 +57,26 @@ public class PetTests {
 
     @Test
     public void updatePet (){
-        String bodyJson = "{\n" +
-                "\"id\":1,\n" +
-                "\"category\":{\n" +
-                "\"id\":1,\n" +
-                "\"name\":\"Cat\"\n" +
-                "},\n" +
-                "\"name\":\"Cat\",\n" +
-                "\"photoUrls\":[\n" +
-                "\"string\"\n" +
-                "],\n" +
-                "\"tags\":[\n" +
-                "{\n" +
-                "\"id\":1,\n" +
-                "\"name\":\"Cat\"\n" +
-                "}\n" +
-                "],\n" +
-                "\"status\":\"available\"\n" +
-                "}";
-
-        RequestSpecification request = new RequestSpecBuilder()
-                .setBaseUri("https://petstore.swagger.io/v2")
-                .build();
-
-        given(request).body(bodyJson).contentType(ContentType.JSON).log().uri().
-                when().post("/pet").
+        given()
+                .body("{\n" +
+                        "\"id\":1,\n" +
+                        "\"category\":{\n" +
+                        "\"id\":1,\n" +
+                        "\"name\":\"Cat\"\n" +
+                        "},\n" +
+                        "\"name\":\"Cat\",\n" +
+                        "\"photoUrls\":[\n" +
+                        "\"string\"\n" +
+                        "],\n" +
+                        "\"tags\":[\n" +
+                        "{\n" +
+                        "\"id\":1,\n" +
+                        "\"name\":\"Cat\"\n" +
+                        "}\n" +
+                        "],\n" +
+                        "\"status\":\"available\"\n" +
+                        "}")
+                .post("/pet").
                 then().log().body().
                 assertThat().
                 body("id", equalTo(1)).
@@ -90,8 +87,6 @@ public class PetTests {
     @Test
     public void deletePetById(){
         given()
-                .log().all()
-                .baseUri("https://petstore.swagger.io/v2")
                 .delete("/pet/1")
                 .then()
                 .log().all()
@@ -99,4 +94,17 @@ public class PetTests {
                 .statusCode(200);
 
     }
+
+    @Test
+    public void getPetById2(){
+        int petId = 666;
+       given()
+                .get(PetEndpoint.GET_PET, petId)
+                .then()
+                .log().all()
+                .body("id", anyOf (is (1), is (petId)))
+                .statusCode(200);
+    }
+
+
 }
